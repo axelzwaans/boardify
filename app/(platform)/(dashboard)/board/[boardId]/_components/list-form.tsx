5fde496d4969fd03a999d002a554d22d08ab4c4c
Axel Zwaans <axelzwaans@hotmail.com>
2024-02-28 09:22:50 +0000

    Edit list form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { Plus, XIcon } from "lucide-react";
-import { useState, useRef, ElementRef, KeyboardEvent } from "react";
-import { ListWrapper } from "./list-wrapper";
-import { useEventListener, useOnClickOutside } from "usehooks-ts";
-import { FormInput } from "@/components/form/form-input";
-import { useParams, useRouter } from "next/navigation";
-import { FormSubmit } from "@/components/form/form-submit";
-import { Button } from "@/components/ui/button";
-import { useAction } from "@/hooks/use-action";
-import { createList } from "@/actions/create-list";
 import { toast } from "sonner";
+import { Plus, X } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState, useRef, ElementRef } from "react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+
+import { useAction } from "@/hooks/use-action";
+import { Button } from "@/components/ui/button";
+import { createList } from "@/actions/create-list";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
+
+import { ListWrapper } from "./list-wrapper";
 
 export const ListForm = () => {
   const router = useRouter();
-
   const params = useParams();
 
   const formRef = useRef<ElementRef<"form">>(null);
@@ -26,7 +27,7 @@ export const ListForm = () => {
     setIsEditing(true);
     setTimeout(() => {
       inputRef.current?.focus();
-    }, 0);
+    });
   };
 
   const disableEditing = () => {
@@ -35,9 +36,9 @@ export const ListForm = () => {
 
   const { execute, fieldErrors } = useAction(createList, {
     onSuccess: (data) => {
-      toast.success(`"List ${data.title}" created!`);
+      toast.success(`List "${data.title}" created`);
       disableEditing();
-      router.refresh;
+      router.refresh();
     },
     onError: (error) => {
       toast.error(error);
@@ -57,7 +58,10 @@ export const ListForm = () => {
     const title = formData.get("title") as string;
     const boardId = formData.get("boardId") as string;
 
-    execute({ title, boardId });
+    execute({
+      title,
+      boardId,
+    });
   };
 
   if (isEditing) {
@@ -72,14 +76,14 @@ export const ListForm = () => {
             ref={inputRef}
             errors={fieldErrors}
             id="title"
-            className="text-sm px-2 py-1 h-7 font-medium border transparent hover:border-input focus:border-input transition"
-            placeholder="Enter list title"
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            placeholder="Enter list title..."
           />
           <input hidden value={params.boardId} name="boardId" />
           <div className="flex items-center gap-x-1">
             <FormSubmit>Add list</FormSubmit>
             <Button onClick={disableEditing} size="sm" variant="ghost">
-              <XIcon className="h-4 w-5" />
+              <X className="h-5 w-5" />
             </Button>
           </div>
         </form>
