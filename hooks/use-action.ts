6e778a6f58f396b8b59bdbc0f0687368834bdeba
Axel Zwaans <axelzwaans@hotmail.com>
2024-02-28 09:29:20 +0000

    Edit use action file

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback } from "react";
 
-import { ActionState, fieldErrors } from "@/lib/create-safe-action";
+import { ActionState, FieldErrors } from "@/lib/create-safe-action";
 
 type Action<TInput, TOutput> = (
   data: TInput
@@ -17,7 +17,7 @@ export const useAction = <TInput, TOutput>(
   options: UseActionOptions<TOutput> = {}
 ) => {
   const [fieldErrors, setFieldErrors] = useState<
-    fieldErrors<TInput> | undefined
+    FieldErrors<TInput> | undefined
   >(undefined);
   const [error, setError] = useState<string | undefined>(undefined);
   const [data, setData] = useState<TOutput | undefined>(undefined);
